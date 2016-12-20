f4126e0a116a1a7d8f9b469b3045aa272960067a
James Balajan <jbalajan@gmail.com>
2016-12-20 11:44:13 +1100

    Delete Globals.cpp

--- Globals.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by JULIA BALAJAN on 15/12/2016.
-//
-
-#include "Globals.h"
-
-// Contains all the block structures that have fallen down, and are still falling
-std::vector<Structure> s;
